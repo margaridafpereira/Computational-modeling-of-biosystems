75ca14609a571d07d0e5fdc23953fa520c7b5bd8
margaridafpereira <148448066+margaridafpereira@users.noreply.github.com>
2024-09-24 21:39:20 +0100

    Delete readfasta.R

--- readfasta.R
+++
@@ -1,3 +0,0 @@
-pinguim<-read.fasta(file = "NC_021474.1.fasta") #leitura de arquivo no formato FASTA
-pinguimseq<-pinguim[[1]] #pinguimseq contém a sequência do genoma - criação de um vetor
-pinguimSeqString <- c2s(pinguimseq)
